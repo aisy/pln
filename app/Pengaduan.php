8d79f6e0c3809ce12ec1e2abcb9822a20c74fba2
Aisy Muhammad <aisymuhammad@gmail.com>
2017-12-25 10:01:22 +0700

    add not fix bug (because im move to other PC

--- app/Pengaduan.php
+++ app/Pengaduan.php
@@ -12,5 +12,5 @@ class Pengaduan extends Model
     //field yang bisa di isi
     // protected $fillable = ['id_kategori', 'isi'];
     // protected $fillable = ['kategori_id','nama_lengkap','alamat','rt','rw','kelurahan','kecamatan','no_ktp','no_telp','layanan','peruntukan','daya'];
-    protected $fillable = ['kategori_id','nama_lengkap','user_id','alamat','keluhan','verifikasi','status','tindak_lanjut'];
+    protected $fillable = ['kategori_id','nama_lengkap','user_id','alamat','keluhan','lain_lain','verifikasi','status','tindak_lanjut'];
 }
--- resources/views/pengaduan/1_1.blade.php
+++ resources/views/pengaduan/1_1.blade.php
@@ -58,34 +58,34 @@
 									<option value="penghuni kosong">penghuni kosong</option>
 									<option value="perluasan jaringan(izin penanaman tiang)">perluasan jaringan(izin penanaman tiang)</option>
 									<option value="permintaan paska bayar">permintaan paska bayar</option>
-									<option value="lain_lain">Lain - lain</option>
+									<option value="lain-lain">Lain - lain</option>
 								@elseif ($kategori->id == 2)
 									<option value="belum terpasang">belum terpasang</option>
 									<option value="pagar terkunci">pagar terkunci</option>
 									<option value="perluasan jaringan(izin penanaman tiang)">perluasan jaringan(izin penanaman tiang)</option>
 									<option value="permintaan paska bayar">permintaan paska bayar</option>
 									<option value="revisi daya">revisi daya</option>
-									<option value="lain_lain">Lain - lain</option>
+									<option value="lain-lain">Lain - lain</option>
 								@elseif ($kategori->id == 6)
 									<option value="manual periksa">manual periksa</option>
 									<option value="tidak bisa beli token">tidak bisa beli token</option>
 									<option value="tidak bisa entry keypad">tidak bisa entry keypad</option>
 									<option value="boros(kwh meter)">boros(kwh meter)</option>
-									<option value="lain_lain">Lain - lain</option>
+									<option value="lain-lain">Lain - lain</option>
 								@elseif ($kategori->id == 4)
 									<option value="angka stan meter tidak sesuai">angka stan meter tidak sesuai</option>
 									<option value="tagihan melunjak">tagihan melunjak</option>
 									<option value="petugas tidak pernah baca meter">petugas tidak pernah baca meter</option>
 									<option value="pintu pagar terkunci">pintu pagar terkunci</option>
-									<option value="lain_lain">Lain - lain</option>
+									<option value="lain-lain">Lain - lain</option>
 								@elseif ($kategori->id == 5)
 									<option value="belum tersambung">belum tersambung</option>
 									<option value="revisi angsuran">revisi angsuran</option>
-									<option value="lain_lain">Lain - lain</option>
+									<option value="lain-lain">Lain - lain</option>
 								@elseif($kategori->id==3)	
 									<option value="keterlambatan bulan pembayaran">keterlambatan bulan pembayaran</option>
 									<option value="tidak bisa membayar rekening">tidak bisa membayar rekening</option>
-									<option value="lain_lain">Lain - lain</option>
+									<option value="lain-lain">Lain - lain</option>
 								@endif
 
 							</select>
@@ -150,7 +150,7 @@
 
 	//Lain lain
 		document.getElementById('keluhan').addEventListener('change', function(){
-			var style = this.value == "lain_lain" ? 'block' : 'none';
+			var style = this.value == "lain-lain" ? 'block' : 'none';
 			document.getElementById('lain2').style.display = style;
 		}); 
 </script>
